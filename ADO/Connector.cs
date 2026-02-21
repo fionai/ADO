cfa07e0889abaf37e1c1812826a459fd290ae20a
Ada <Ada@PROBOOK>
2026-02-21 16:34:06 +0700

    Add project files.

---
+++ ADO/Connector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace ADO
+{
+	internal class Connector
+	{
+		string connection_string;
+		SqlConnection connection;
+		public Connector(string connection_string)
+		{
+			this.connection_string = connection_string;
+			this.connection = new SqlConnection(connection_string);
+		}
+		public void Select(string cmd)
+		{
+			connection.Open();
+			SqlCommand command = new SqlCommand(cmd, connection);
+			SqlDataReader reader = command.ExecuteReader();
+			while (reader.Read())
+			{
+				//Console.WriteLine($"{reader[0]}\t{reader[1]} {reader[2]}");
+				for (int i = 0; i < reader.FieldCount; i++)
+				{
+					Console.Write(reader[i].ToString().PadRight(28));
+				}
+				Console.WriteLine();
+			}
+
+			reader.Close();
+			connection.Close();
+		}
+	}
+}
---
+++ ADO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace ADO
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_SPU_411;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			SqlConnection connection = new SqlConnection(connection_string);
+			string cmd = "SELECT title,year,first_name,last_name  FROM Movies JOIN Directors ON (director=director_id)";
+
+
+			Connector connector = new Connector(connection_string);
+			connector.Select(cmd);
+		}
+	}
+}
